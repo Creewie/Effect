4294276b286c83c05f28064f722dfab32e2bd585
Creewie <tabero2006@gmail.com>
2024-01-04 08:58:18 +0100

    Files

---
+++ useEffect.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+
+const UseEffectComponent = () => {
+    
+    return(
+        <View>
+            <Text>UseEffectComponent</Text>
+        </View>
+    )
+
+}
+export default UseEffectComponent;
