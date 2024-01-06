6ec3f01fbd59d25c231352aadf7a4a234f9c4568
Creewie <tabero2006@gmail.com>
2024-01-06 14:22:41 +0100

    time

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import UseEffectComponent from './useEffect';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [refresh, setRefresh] = useState()
+
+  function showTime(){
+    const now = new Date();
+    const currentTime = now.toLocaleTimeString();
+    setRefresh(currentTime)
+  }
+  setInterval(showTime, 1000);
+
   return (
     <View style={styles.container}>
-      <UseEffectComponent/>
+      <Text style={styles.font}>Aktualny czas: {refresh}</Text>
     </View>
   );
 }
@@ -15,6 +24,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  font: {
+    fontSize: 35,
+  }
 });
--- zegarekMobilny.js
+++ zegarekMobilny.js
@@ -1,8 +1,13 @@
-function showTime(){
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function showTime(){
     const now = new Date();
     const currentTime = now.toLocaleTimeString();
-    console.clear();
-    console.log(`Aktualny czas: ${currentTime}`);
+    // console.clear();
+    // console.log(`Aktualny czas: ${currentTime}`);
+    <View>
+        <Text>Aktualny czas:{currentTime}</Text>
+    </View>
 }
 
 setInterval(showTime, 1000);
